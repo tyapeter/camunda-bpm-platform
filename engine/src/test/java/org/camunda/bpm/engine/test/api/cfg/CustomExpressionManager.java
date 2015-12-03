001e0cedda97c435d0970f6531d57941ca2de131
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2015-12-03 15:30:13 +0100

    engine.test.el

--- engine/src/test/java/org/camunda/bpm/engine/test/api/cfg/CustomExpressionManager.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/api/cfg/CustomExpressionManager.java
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.camunda.bpm.engine.test.el;
+package org.camunda.bpm.engine.test.api.cfg;
 
 import java.util.List;
 
--- engine/src/test/java/org/camunda/bpm/engine/test/api/cfg/CustomExpressionManagerTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/api/cfg/CustomExpressionManagerTest.java
@@ -11,7 +11,7 @@
  * limitations under the License.
  */
 
-package org.camunda.bpm.engine.test.el;
+package org.camunda.bpm.engine.test.api.cfg;
 
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngineConfiguration;
