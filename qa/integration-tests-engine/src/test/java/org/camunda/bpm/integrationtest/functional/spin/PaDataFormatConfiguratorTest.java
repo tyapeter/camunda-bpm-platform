fcd144c35de94bced0c423ba1329f1551dbaca12
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2015-12-11 15:29:51 +0100

    test case for implicit value update

--- qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/functional/spin/PaDataFormatConfiguratorTest.java
+++ qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/functional/spin/PaDataFormatConfiguratorTest.java
@@ -17,10 +17,11 @@ import java.util.Date;
 
 import org.camunda.bpm.application.ProcessApplicationContext;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.variable.Variables;
 import org.camunda.bpm.engine.variable.Variables.SerializationDataFormats;
 import org.camunda.bpm.engine.variable.value.ObjectValue;
-import org.camunda.bpm.engine.variable.value.SerializationDataFormat;
+import org.camunda.bpm.integrationtest.functional.spin.dataformat.ImplicitObjectValueUpdateDelegate;
 import org.camunda.bpm.integrationtest.functional.spin.dataformat.JsonDataFormatConfigurator;
 import org.camunda.bpm.integrationtest.functional.spin.dataformat.JsonSerializable;
 import org.camunda.bpm.integrationtest.util.AbstractFoxPlatformIntegrationTest;
@@ -59,8 +60,10 @@ public class PaDataFormatConfiguratorTest extends AbstractFoxPlatformIntegration
         .addClass(TestContainer.class)
         .addClass(ReferenceStoringProcessApplication.class)
         .addAsResource("org/camunda/bpm/integrationtest/oneTaskProcess.bpmn")
+        .addAsResource("org/camunda/bpm/integrationtest/functional/spin/implicitUpdate.bpmn")
         .addClass(JsonDataFormatConfigurator.class)
         .addClass(JsonSerializable.class)
+        .addClass(ImplicitObjectValueUpdateDelegate.class)
         .addAsServiceProvider(DataFormatConfigurator.class, JsonDataFormatConfigurator.class)
         .addAsLibraries(DeploymentHelper.getSpinJacksonJsonDataFormat());
   }
@@ -109,6 +112,41 @@ public class PaDataFormatConfiguratorTest extends AbstractFoxPlatformIntegration
 
     String serializedValue = objectValue.getValueSerialized();
     String expectedSerializedValue = jsonSerializable.toExpectedJsonString();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    JsonNode actualJsonTree = objectMapper.readTree(serializedValue);
+    JsonNode expectedJsonTree = objectMapper.readTree(expectedSerializedValue);
+    // JsonNode#equals makes a deep comparison
+    Assert.assertEquals(expectedJsonTree, actualJsonTree);
+  }
+
+  @Test
+  public void testImplicitObjectValueUpdate() throws JsonProcessingException, IOException {
+    Date date = new Date(ONE_DAY_IN_MILLIS * 10); // 10th of January 1970
+    JsonSerializable jsonSerializable = new JsonSerializable(date);
+
+    ProcessInstance pi = runtimeService.startProcessInstanceByKey("implicitUpdate");
+
+    try {
+      ProcessApplicationContext.setCurrentProcessApplication(ReferenceStoringProcessApplication.INSTANCE);
+      runtimeService.setVariable(pi.getId(),
+        ImplicitObjectValueUpdateDelegate.VARIABLE_NAME,
+        Variables.objectValue(jsonSerializable).serializationDataFormat(SerializationDataFormats.JSON).create());
+    } finally {
+      ProcessApplicationContext.clear();
+    }
+
+    Task task = taskService.createTaskQuery().processInstanceId(pi.getId()).singleResult();
+    // triggering an implicit update of the object value variable
+    taskService.complete(task.getId());
+
+    ObjectValue objectValue = runtimeService.getVariableTyped(pi.getId(),
+        ImplicitObjectValueUpdateDelegate.VARIABLE_NAME,
+        false);
+
+    ImplicitObjectValueUpdateDelegate.addADay(jsonSerializable);
+    String serializedValue = objectValue.getValueSerialized();
+    String expectedSerializedValue = jsonSerializable.toExpectedJsonString(JsonDataFormatConfigurator.DATE_FORMAT);
 
     ObjectMapper objectMapper = new ObjectMapper();
     JsonNode actualJsonTree = objectMapper.readTree(serializedValue);
