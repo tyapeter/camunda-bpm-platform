60431c539757356e9b3ae54af0fef7feb69cfd43
Frank Langelage <Frank.Langelage@osnanet.de>
2016-03-25 22:23:55 +0100

    IBM Informix support.

---
+++ engine/src/main/resources/org/camunda/bpm/engine/db/drop/activiti.informix.drop.decision.history.sql
@@ -0,0 +1,3 @@
+drop table if exists ACT_HI_DECINST;
+drop table if exists ACT_HI_DEC_IN;
+drop table if exists ACT_HI_DEC_OUT;
---
+++ engine/src/main/resources/org/camunda/bpm/engine/db/drop/activiti.informix.drop.history.sql
@@ -0,0 +1,11 @@
+drop table if exists ACT_HI_PROCINST;
+drop table if exists ACT_HI_ACTINST;
+drop table if exists ACT_HI_VARINST;
+drop table if exists ACT_HI_TASKINST;
+drop table if exists ACT_HI_DETAIL;
+drop table if exists ACT_HI_COMMENT;
+drop table if exists ACT_HI_ATTACHMENT;
+drop table if exists ACT_HI_OP_LOG;
+drop table if exists ACT_HI_INCIDENT;
+drop table if exists ACT_HI_JOB_LOG;
+drop table if exists ACT_HI_BATCH;
